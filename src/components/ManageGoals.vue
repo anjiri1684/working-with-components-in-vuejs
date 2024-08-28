99a5d11d51d2f93ae1c049929f75c25ae3fb5ed2
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-28 18:06:28 +0300

    "Wrap error-alert component with teleport to 'body' element"

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -3,11 +3,13 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="addGoal">Add Goal</button>
-    <error-alert v-if="inputIsInvalid">
-      <p>Input is invalid</p>
-      <p>Please enter at least few characters....</p>
-      <button @click="confirmError">Okay</button>
-    </error-alert>
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <p>Input is invalid</p>
+        <p>Please enter at least few characters....</p>
+        <button @click="confirmError">Okay</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
