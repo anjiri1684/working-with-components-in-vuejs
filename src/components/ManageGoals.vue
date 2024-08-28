cc5cc3a1129a4eb5d14ca782cb931d3afc67c478
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-28 15:22:32 +0300

    "Removed HOW-TO-USE.pdf file, updated App.vue to wrap component in keep-alive, and modified ManageGoals.vue to add input field, button, and error alert functionality"

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,3 +1,33 @@
 <template>
-  <h2>Manage Goals</h2>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="addGoal">Add Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <p>Input is invalid</p>
+      <p>Please enter at least few characters</p>
+    </error-alert>
+  </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  components: { ErrorAlert },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    addGoal() {
+      const enteredValue = this.$refs.goal.value;
+
+      if (enteredValue.trim() === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+  },
+};
+</script>
